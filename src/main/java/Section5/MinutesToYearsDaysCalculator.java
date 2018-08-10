258f236a9ae02d9b52d6823c11f76f842914c2f3
Grace Williams <grace.williams@innovateuk.gov.uk>
2018-08-10 09:53:00 +0100

    Section 5: Minutes to years calculator update

--- src/main/java/Section5/MinutesToYearsDaysCalculator.java
+++ src/main/java/Section5/MinutesToYearsDaysCalculator.java
@@ -4,20 +4,19 @@ public class MinutesToYearsDaysCalculator {
 
     public static void printYearsAndDays(long minutes){
 
-        long XX = minutes;
-        long days = minutes / 60 / 24; // number of days
-        long YY = days / 365; // number of years
-        long ZZ = days % 365; // days left
+        long days = (minutes/60)/ 24;
+        long years = days / 365;
+        long remainingDays= days % 365;
 
         if (minutes < 0){
             System.out.println("Invalid Value");
         } else {
-            System.out.println(XX + " min = " + YY + " y and " + ZZ + " d");
+            System.out.println(minutes + " min = " + years + " y and " + remainingDays + " d");
         }
     }
 
     public static void main(String[] args) {
-        printYearsAndDays(-525600);
+        printYearsAndDays(525600);
     }
 }
 
