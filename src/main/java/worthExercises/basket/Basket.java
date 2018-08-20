74905a80bf6d053cf027c736b190d02022a1ecb3
Grace Williams <grace.williams@innovateuk.gov.uk>
2018-08-20 15:36:36 +0100

    Added the goal for basket exercise

--- src/main/java/worthExercises/basket/Basket.java
+++ src/main/java/worthExercises/basket/Basket.java
@@ -56,3 +56,14 @@ public class Basket {
         this.apples = apples;
     }
 }
+
+
+// Goal:
+// The shopping basket contains an order for apples (25p), bananas (18p) , oranges (35p),
+// carrots (65p per kilo), courgettes (20p) and peppers (55p).
+//
+// Current deals are 3 for 2 on peppers. 30% off apples when you buy more than 3 apples,
+// 50% off apples when you buy more than 7 apples. Get a free courgette if you buy more than 5 fruit.
+// Get a free kilo of carrots for every 4 items of fruit bought.
+//
+// Implement the method Checkout.calculatePrice() to return the correct price of the basket.
--- src/main/java/worthExercises/basket/Checkout.java
+++ src/main/java/worthExercises/basket/Checkout.java
@@ -81,3 +81,14 @@ public class Checkout {
         return basket.getPeppers() * .55;
     }
 }
+
+
+// Goal:
+// The shopping basket contains an order for apples (25p), bananas (18p) , oranges (35p),
+// carrots (65p per kilo), courgettes (20p) and peppers (55p).
+//
+// Current deals are 3 for 2 on peppers. 30% off apples when you buy more than 3 apples,
+// 50% off apples when you buy more than 7 apples. Get a free courgette if you buy more than 5 fruit.
+// Get a free kilo of carrots for every 4 items of fruit bought.
+//
+// Implement the method Checkout.calculatePrice() to return the correct price of the basket.
